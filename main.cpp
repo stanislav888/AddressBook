cf94dc4df433c4182dab29721681df299b7f85f9
stanislav888 <stanislav888@github.com>
2017-05-11 17:41:43 +0700

    Changed registry company name

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	a.setOrganizationName( "Amon-Ra" ); // Using as settings folder in registry
+	a.setOrganizationName( "stanislav888@github.com" ); // Using as settings folder in registry
 	AddressBookMainWindow w;
 	w.show();
 
